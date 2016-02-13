db3d74d4b3b53cd4dcafa5f4c23acd8b7449dbcf
NJWewala <njc.wewala@gmail.com>
2016-02-12 19:30:00 -0500

    done

--- SYSC3303_Itrration1/src/ProfileData.java
+++ SYSC3303_Itrration1/src/ProfileData.java
@@ -1,12 +1,12 @@
 
 public class ProfileData {
 	private final int serverPort = 1025;
-	private final int intermediatePort = 1024;
+	private final int errorSimulator = 1024;
 	
 	public int getServerPort() {
 		return serverPort;
 	}
 	public int getIntermediatePort() {
-		return intermediatePort;
+		return errorSimulator;
 	}
 }
