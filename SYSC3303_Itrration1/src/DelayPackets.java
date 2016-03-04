8f81c65d0aca281efe9be022d84f15d13a3bdc14
veenarose <mathew.cateyes@gmail.com>
2016-03-03 21:53:16 -0500

    Added New class header for handling Delay

---
+++ SYSC3303_Itrration1/src/DelayPackets.java
@@ -0,0 +1,18 @@
+/*
+ * Class that is used to delay packets handling synchronization
+ */
+public class DelayPackets implements Runnable{
+	
+	
+	
+	/*
+	 * (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
